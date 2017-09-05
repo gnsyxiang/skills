2f2e977c6a669e9976a46e46f0d98604382fa46c
gnsyxiang <gnsyxiang@163.com>
2017-09-05 14:25:19 +0800

    feat: add array to do parametric problem

---
+++ src/c/array/array.h
@@ -0,0 +1,34 @@
+/****************************************************************
+ * Filename: array.h
+ * -------------------------------
+ * Copyright(C),
+ * Author: zhenquan.qiu
+ * Version: V1.0.0
+ * Last modified: 05/09 2017 14:23
+ * Description:
+ * 
+ * Change Log:
+ * NO.	Author		    Date		Modified
+ * 00	zhenquan.qiu	05/09 2017
+ ****************************************************************/
+
+#ifndef _ARRAY_H_
+#define _ARRAY_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef ARRAY_GB
+#define ARRAY_EX extern
+#else
+#define ARRAY_EX
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* end _ARRAY_H_ */
+
